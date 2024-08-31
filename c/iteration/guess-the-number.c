2e381bb76c903065100cf6cd9f54df8994278d38
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-08-31 22:10:53 +0530

    20240831

---
+++ c/iteration/guess-the-number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+    int low = 1, high = 100, mid, attempt = 1;
+    char response;
+    printf("Guess a number between 1 and 100, then, press enter to continue...");
+    getchar();
+    mid = (low + high) / 2;
+    printf("Is the number %d (y/n)? ", mid);
+    scanf("%c", &response);
+    if (response == 'y')
+    {
+        printf("Wow! I have guessed your number in %d attempt(s).\n", attempt);
+    }
+    return 0;
+}
