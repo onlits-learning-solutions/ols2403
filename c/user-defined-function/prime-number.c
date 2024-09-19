6f0fd42160466e6145e09d90f5fe440b805ab22e
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-09-19 22:20:41 +0530

    20240919

---
+++ c/user-defined-function/prime-number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>              // Function declaration for library function
+
+int countFactor(int n);         // Function declaration for user-defined function
+
+// Function Definition
+int main(int argc, char const *argv[])
+{
+    int n, factors;
+    printf("Enter number: ");               // Function call to a library function
+    scanf("%d", &n);                        // Function call to a libray function
+    factors = countFactor(n);              // Function call to a user-defined function
+    if(factors == 2)
+        printf("%d is a prime number.\n", n);
+    else
+        printf("%d is not a prime number.\n", n);
+    return 0;
+}
+
+// Function Definition
+int countFactor(int n)
+{
+    int factor = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            factor++;
+        }
+    }
+    return factor;
+}
+
+/*
+Function is a self-contained block of code to carry out a specific task.
+*/
