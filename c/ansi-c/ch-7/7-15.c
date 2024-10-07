78eb70b2b740c01bbfecca80ae52017350156ccc
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-10-07 22:06:25 +0530

    20241007

---
+++ c/ansi-c/ch-7/7-15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            if (i == 3 && j == 3)
+                printf(" O ");
+            else
+                printf(" S ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
