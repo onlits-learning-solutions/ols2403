03463680a8b3d9e1bc7b1978c36c56a4f0ac2714
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-10-26 10:52:38 +0530

    20241026

--- c/structure/holiday.c
+++ c/structure/holiday.c
@@ -12,60 +12,25 @@ typedef struct date
 
 typedef struct holiday
 {
-    int id;
     char name[50];
     DATE date;
     char description[100];
 } HOLIDAY;
 
-void displayHolidayList(HOLIDAY *holiday, int size);
-void newHoliday(HOLIDAY *holiday, int size);
-void displayHolidayById(HOLIDAY *holiday, int size, int id);
+void newHoliday(HOLIDAY *holiday);
+void displayHoliday(HOLIDAY *holiday);
 
 int main(int argc, char const *argv[])
 {
-    HOLIDAY holiday[MAX];
-    int ch, size = 0;
-
-    while (1)
-    {
-        printf("MENU\n");
-        printf("1. Display Holiday List\n");
-        printf("2. New Holiday\n");
-
-        printf("Enter choice [0 to exit]: ");
-        scanf("%d%*c", &ch);
-
-        switch (ch)
-        {
-        case 0:
-            printf("Bye!\n");
-            exit(0);
-            break;
-        case 1:
-            displayHolidayList(holiday, size);
-            break;
-        case 2:
-            newHoliday(holiday, size);
-            break;
-        default:
-            printf("\nInvalid option!\n\n");
-            break;
-        }
-    }
+    HOLIDAY holiday;
+    newHoliday(&holiday);
+    printf("-------------------------------\n");
+    displayHoliday(&holiday);
     return 0;
 }
 
-void displayHolidayList(HOLIDAY *holiday, int size)
+void newHoliday(HOLIDAY *holiday)
 {
-    printf("\n\nHOLIDAY LIST\n");
-    printf("Date\tName\tDescription\n");
-}
-
-void newHoliday(HOLIDAY *holiday, int size)
-{
-    int id = 1;
-    holiday->id = id;
     printf("Enter holiday name: ");
     scanf("%[^\n]", holiday->name);
     printf("Enter date (dd/mm/yyyy): ");
@@ -74,16 +39,9 @@ void newHoliday(HOLIDAY *holiday, int size)
     scanf("%[^\n]", holiday->description);
 }
 
-void displayHolidayById(HOLIDAY *holiday, int size, int id)
+void displayHoliday(HOLIDAY *holiday)
 {
-    // for (int i = 0; i < size; i++)
-    // {
-    //     if (holiday[i] == id)
-    //     {
-    //         printf("Id: %d\n", holiday->id);
-    //         printf("Name: %s\n", holiday->name);
-    //         printf("Date: %2d/%2d/%d\n", holiday->date.dd, holiday->date.mm, holiday->date.yyyy);
-    //         printf("Description: %s\n", holiday->description);
-    //     }
-    // }
+    printf("Holiday Name: %s\n", holiday->name);
+    printf("Date: %d/%d/%d\n",holiday->date.dd, holiday->date.mm, holiday->date.yyyy);
+    printf("Description: %s\n", holiday->description);
 }
