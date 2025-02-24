ef9d0c3c51503606a3fb67c07c5c8dc47407fd71
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2025-02-24 09:59:13 +0530

    20250224

--- java/project/resume-builder/src/test/TestContactInformation.java
+++ java/project/resume-builder/src/test/TestContactInformation.java
@@ -1,8 +1,8 @@
-// import ols2403.resumebuilder;
+import ols2403.resumebuilder;
 
 public class TestContactInformation {
     public static void main(String[] args) {
-        // ContactInformation contactInformation = new ContactInformation();
+        ContactInformation contactInformation = new ContactInformation();
     
     }
 }
